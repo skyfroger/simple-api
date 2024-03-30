b43c16128bfb2906c2b87cc361c35f2999f1ffc8
skyfroger <skyfroger@gmail.com>
2024-03-30 17:36:36 +0300

    feat: хранение 1023 точек на холсте

--- routers/canvas.py
+++ routers/canvas.py
@@ -2,13 +2,16 @@ from typing import Union, List
 from pydantic import BaseModel
 
 from fastapi import APIRouter
+
 router = APIRouter(tags=["Холст"])
+
 
 # модель точки на холсте
 class CanvasPoint(BaseModel):
-    x: Union[int|float]
-    y: Union[int|float]
+    x: Union[int | float]
+    y: Union[int | float]
     color: Union[int, str]
+
 
 class CanvasContent(BaseModel):
     canvas: List[CanvasPoint]
@@ -16,15 +19,18 @@ class CanvasContent(BaseModel):
 
 pixels_field = []  # массив с координатами и цветами пикселей
 
+
 @router.post("/canvas", response_model=CanvasPoint)
 async def add_point_to_canvas(point: CanvasPoint):
-    if len(pixels_field) < 256:
+    if len(pixels_field) < 1024:
         pixels_field.append(point)
     return point
+
 
 @router.get("/canvas", response_model=CanvasContent)
 async def get_canvas_content():
     return {"canvas": pixels_field}
+
 
 @router.delete("/canvas", response_model=CanvasContent)
 async def clear_canvas():
