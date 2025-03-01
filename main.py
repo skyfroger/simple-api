ff6f390f21feb0c15c650704c866ccde551d0a7d
skyfroger <skyfroger@gmail.com>
2025-03-01 21:06:51 +0300

    fix: переход на другую CDN в Swagger UI

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 
 from routers import ball8, is_even, canvas, qr
 
@@ -8,6 +13,8 @@ app = FastAPI(
     version="1.0.0",
     description="Набор тестовых API",
     root_path="/api",
+    docs_url=None,
+    redoc_url=None
 )
 
 app.add_middleware(
@@ -22,3 +29,22 @@ app.include_router(ball8.router)
 app.include_router(is_even.router)
 app.include_router(canvas.router)
 app.include_router(qr.router)
+
+"""
+чиню то, что не сломано
+Swagger UI будет брать js и css из unpkg
+"""
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
