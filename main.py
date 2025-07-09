3e9019e9e6163ace4c83352f95c73b81aad81fa9
skyfroger <skyfroger@gmail.com>
2025-07-09 22:03:49 +0300

    ref: удален фикс

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.openapi.docs import (
-    get_swagger_ui_html,
-    get_swagger_ui_oauth2_redirect_html,
-    get_redoc_html
-)
 
-from routers import ball8, is_even, canvas, qr
+
+from routers import ball8, is_even, canvas, qr, quote
 
 app = FastAPI(
     title="Simple API",
     version="1.0.0",
     description="Набор тестовых API",
     root_path="/api",
-    docs_url=None,
-    redoc_url=None
 )
 
 app.add_middleware(
@@ -29,31 +23,4 @@ app.include_router(ball8.router)
 app.include_router(is_even.router)
 app.include_router(canvas.router)
 app.include_router(qr.router)
-
-
-"""
-чиню то, что не сломано
-Swagger UI будет брать js и css из unpkg
-"""
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - Swagger UI",
-        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
-        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
-    )
-
-
-@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
-async def swagger_ui_redirect():
-    return get_swagger_ui_oauth2_redirect_html()
-
-@app.get("/redoc", include_in_schema=False)
-async def redoc_html():
-    return get_redoc_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - ReDoc",
-        redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
-    )
+app.include_router(quote.router)
